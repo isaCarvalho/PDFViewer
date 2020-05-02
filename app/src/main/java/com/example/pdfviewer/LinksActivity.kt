be9420f1d6aa679542231ee918139ee96f7388e9
isaCarvalho <isabelacarvalhocf@hotmail.com>
2020-05-02 15:05:54 -0300

    Importar dados feito

--- app/src/main/java/com/example/pdfviewer/LinksActivity.kt
+++ app/src/main/java/com/example/pdfviewer/LinksActivity.kt
@@ -1,9 +1,13 @@
 package com.example.pdfviewer
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.SearchView
 import android.widget.TextView
 import android.widget.Toast
@@ -11,23 +15,34 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pdfviewer.adapter.LinkAdapter
 import com.example.pdfviewer.database.DatabaseController
+import com.example.pdfviewer.model.Link
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.io.PrintWriter
 import java.lang.Exception
+import java.lang.StringBuilder
 
 class LinksActivity : AppCompatActivity() {
+
+    companion object {
+        const val READ_FILE = 1000
+    }
 
     var viewManager : LinearLayoutManager? = null
     private lateinit var viewAdapter : LinkAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var fabSave : FloatingActionButton
 
+    private lateinit var links : ArrayList<Link>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_links)
 
         val databaseController = DatabaseController(this)
-
-        val links = databaseController.list()
+        links = databaseController.list()
 
         viewManager = LinearLayoutManager(this)
         viewAdapter = LinkAdapter(links)
@@ -54,7 +69,8 @@ class LinksActivity : AppCompatActivity() {
                     {
                         databaseController.insert(url)
 
-                        viewAdapter = LinkAdapter(DatabaseController(this).list())
+                        links = DatabaseController(this).list()
+                        viewAdapter = LinkAdapter(links)
                         recyclerView.adapter = viewAdapter
 
                         findViewById<TextView>(R.id.saveLink).text = null
@@ -86,5 +102,79 @@ class LinksActivity : AppCompatActivity() {
         })
 
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId)
+        {
+            R.id.exportMenu -> {
+                var string = ""
+                links.forEach { elem ->
+                    string += "${elem.url}\n"
+                }
+
+                if (string.isEmpty())
+                    Toast.makeText(this, "Não existem links para exportar", Toast.LENGTH_SHORT).show()
+                else
+                {
+                    var success = true
+                    val path = applicationContext.filesDir
+
+                    val pdfViewerDirectory = File(path, "PDFViewer")
+                    if (!pdfViewerDirectory.exists())
+                        success = pdfViewerDirectory.mkdir()
+
+                    if (success)
+                    {
+                        val file = File(pdfViewerDirectory, "links.txt")
+                        if (!file.exists())
+                            success = file.createNewFile()
+
+                        if (success)
+                            file.appendText(string).also {
+                                Toast.makeText(this, "Links exportados para o armazenamento interno", Toast.LENGTH_SHORT).show()
+                            }
+                    }
+                }
+
+                true
+            }
+
+            R.id.importMenu -> {
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    addCategory(Intent.CATEGORY_OPENABLE)
+
+                    type = "text/plain"
+                }
+
+                startActivityForResult(intent, READ_FILE)
+                true
+            }
+
+            else -> true
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == READ_FILE && resultCode == Activity.RESULT_OK) {
+            data!!.data.also { uri ->
+                val stringBuilder = StringBuilder()
+
+                contentResolver.openInputStream(uri!!).use { inputStream ->
+                    BufferedReader(InputStreamReader(inputStream!!)).use { reader ->
+                        val lines = reader.readLines()
+
+                        lines.forEach { line ->
+                            DatabaseController(this@LinksActivity).insert(line)
+                        }
+
+                        links = DatabaseController(this@LinksActivity).list()
+                        recyclerView.adapter = LinkAdapter(links)
+                    }
+                }
+            }
+        }
     }
 }
